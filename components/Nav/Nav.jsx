8c785259bbbad57a976bf537b9fcac04a1d2ffd9
Tesla_Ambassador <77091499+tesla-ambassador@users.noreply.github.com>
2022-08-05 23:18:02 +0300

    Tried to fix some validations

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
 import {useRouter} from 'next/router'
-import { Image, Shimmer } from 'react-shimmer';
+import Image from 'next/image'
+import logo from '/public/assets/shared/logo.svg'
 import { motion } from 'framer-motion';
 import {Twirl as Hamburger} from 'hamburger-react';
+
 
 export default function Nav(props) {
     const router = useRouter();
@@ -19,26 +21,26 @@ export default function Nav(props) {
         >
             <Link href={'/'} as={'home'} >
                 <a aria-label = 'link to home page'>
-                    <Image src={'/assets/shared/logo.svg'} alt='logo' fallback={<Shimmer height={100} width={100}/>} />
+                    <Image src={logo} alt='logo' />
                 </a>
             </Link>
         </motion.div>
 
         <div className='hidden tab:flex dsk:hidden xdsk:hidden justify-center items-center glassNav h-24 tracking-navTab'>
             <ul className='inline'>
-                <li className='inline mx-5' ><Link href={'/'} as={'home'} ><a className={currentRoute === '/' ? 'active text-white' : 'non-active text-midDark'} >Home</a></Link></li>
-                <li className='inline mx-5' ><Link href={'/destination'} as={'destination'} ><a className={currentRoute === '/destination' ? 'active text-white' : 'non-active text-midDark'} >Destination</a></Link></li>
-                <li className='inline mx-5' ><Link href={'/crew'} as={'crew'} ><a className={currentRoute === '/crew' ? 'active text-white' : 'non-active text-midDark'} >Crew</a></Link></li>
-                <li className='inline mx-5' ><Link href={'/technology'} as={'technology'} ><a className={currentRoute === '/technology' ? 'active text-white' : 'non-active text-midDark'} >Technology</a></Link></li>
+                <li className='inline mx-5' ><Link href={'/'} as={'home'} rel={'preload'} ><a className={currentRoute === '/' ? 'active text-white' : 'non-active text-midDark'} >Home</a></Link></li>
+                <li className='inline mx-5' ><Link href={'/destination'} as={'destination'} rel={'preload'} ><a className={currentRoute === '/destination' ? 'active text-white' : 'non-active text-midDark'} >Destination</a></Link></li>
+                <li className='inline mx-5' ><Link href={'/crew'} as={'crew'} rel={'preload'} ><a className={currentRoute === '/crew' ? 'active text-white' : 'non-active text-midDark'} >Crew</a></Link></li>
+                <li className='inline mx-5' ><Link href={'/technology'} as={'technology'} rel={'preload'} ><a className={currentRoute === '/technology' ? 'active text-white' : 'non-active text-midDark'} >Technology</a></Link></li>
             </ul> 
         </div>
 
         <div className='hidden dsk:flex dsk:px-8 justify-center items-center line relative glassNav h-24 tracking-navTab xdsk:px-12'>
             <ul className='inline'>
-                <li className='inline mx-5' ><Link href={'/'} as={'home'} ><a className={currentRoute === '/' ? 'active text-white' : 'non-active text-midDark'} ><span className='font-bold' >00</span> Home</a></Link></li>
-                <li className='inline mx-5' ><Link href={'/destination'} as={'destination'} ><a className={currentRoute === '/destination' ? 'active text-white' : 'non-active text-midDark'} ><span className='font-bold' >01</span> Destination</a></Link></li>
-                <li className='inline mx-5' ><Link href={'/crew'} as={'crew'} ><a className={currentRoute === '/crew' ? 'active text-white' : 'non-active text-midDark'} ><span className='font-bold' >02</span> Crew</a></Link></li>
-                <li className='inline mx-5' ><Link href={'/technology'} as={'technology'} ><a className={currentRoute === '/technology' ? 'active text-white' : 'non-active text-midDark'} ><span className='font-bold' >03</span> Technology</a></Link></li>
+                <li className='inline mx-5' ><Link href={'/'} as={'home'} rel={'preload'} ><a className={currentRoute === '/' ? 'active text-white' : 'non-active text-midDark'} ><span className='font-bold' >00</span> Home</a></Link></li>
+                <li className='inline mx-5' ><Link href={'/destination'} as={'destination'} rel={'preload'} ><a className={currentRoute === '/destination' ? 'active text-white' : 'non-active text-midDark'} ><span className='font-bold' >01</span> Destination</a></Link></li>
+                <li className='inline mx-5' ><Link href={'/crew'} as={'crew'} rel={'preload'} ><a className={currentRoute === '/crew' ? 'active text-white' : 'non-active text-midDark'} ><span className='font-bold' >02</span> Crew</a></Link></li>
+                <li className='inline mx-5' ><Link href={'/technology'} as={'technology'} rel={'preload'} ><a className={currentRoute === '/technology' ? 'active text-white' : 'non-active text-midDark'} ><span className='font-bold' >03</span> Technology</a></Link></li>
             </ul> 
         </div>
 
